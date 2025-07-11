6f68fd207de59f37d0b576f1cc0d039a169ffdb7
VaibhavOrbit <vaibhav.readme@gmail.com>
2025-07-11 20:51:53 +0530

    add delete button

--- apps/frontend/app/(dashboard)/list/announcements/page.tsx
+++ apps/frontend/app/(dashboard)/list/announcements/page.tsx
@@ -58,15 +58,11 @@ const AnnouncementsListPage = () => {
       
       <td>
         <div className="flex items-center gap-2">
-          <Link href={`/list/teachers/${item.id}`}>
-          <button className="w-7 h-7 flex items-center justify-center rounded-full">
-              <Image src="/view.png" alt="" width={16} height={16}/>
-          </button>
-          </Link>
           {role === "admin" && (
-              <button className="w-7 h-7 flex items-center justify-center rounded-full">
-              <Image src="/delete.png" alt="" width={16} height={16}/>  
-          </button>
+            <>
+            <FormModal table="announcement" type="update" data={item}/>
+            <FormModal table= "announcement" type = "delete" id={item.id}/>
+            </>
           )} 
         </div>
       </td>
@@ -79,8 +75,8 @@ const AnnouncementsListPage = () => {
     <div className="bg-white rounded-md flex-1 mt-0 m-4 p-4">
       {/* TOP */}
       <div className="flex justify-between items-center ">
-        <h1 className="text-lg font-semibold md:block hidden">All Event</h1>
-        <div className="flex flex-col md:flex-row items-center gap-4 w-full md:w-auto">
+        <h1 className="text-lg font-semibold md:block hidden">All announcements</h1>
+        <div className="flex flex-col md:flex-row items-center gap-4 w-full md:w-auto ">
           <TableSearch/>
           < div className="flex items-center gap-4 self-end ">
             <button className="w-8 h-8 flex items-center justify-center rounded-full bg-yellow-300">
