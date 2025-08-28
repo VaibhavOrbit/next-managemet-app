a13ebf33bd17ac443ef8636bb6652567200d2ed0
VaibhavOrbit <vaibhav.readme@gmail.com>
2025-08-28 21:22:26 +0530

    resolve Prisma Client error

--- apps/frontend/app/(dashboard)/list/teachers/page.tsx
+++ apps/frontend/app/(dashboard)/list/teachers/page.tsx
@@ -7,9 +7,8 @@ import { role, teachersData } from "@/app/lib/data"
 import Image from "next/image"
 import Link from "next/link"
 import { prismaClient } from "@repo/db/client";
-// import { Class, Prisma, Subject, Teacher } from "@repo/db/client";
-
-import { Subject } from "@repo/db/";
+import { Teacher, Subject, Class } from "@prisma/client"; 
+import { AwardIcon } from "lucide-react"
 
 
 type  TeacherList  = Teacher & {subjects: Subject[]} & {classes: Class[]}
@@ -50,48 +49,56 @@ const columns = [
   }
 ];
 
-
-
-const TeacherListPage = () => {
-
-  const renderRow = (item: Teacher) => (
+ const renderRow = (item: TeacherList) => (
     <tr
-     key={item.id}
-     className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-blue-200"
-     >
-      <td className="flex items-center  gap-4 p-4">
+      key={item.id}
+      className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-lamaPurpleLight"
+    >
+      <td className="flex items-center gap-4 p-4">
         <Image
-            src={item.photo}
-            alt=""
-            width={40} 
-            height={40} 
-            className="md:hidden xl:block w-10 h-10 rounded-full object-cover"
-            />
-            <div className="flex flex-col"> 
-              <h3 className="font-semibold">{item.name}</h3>
-              <p className="text-xs text-gray-500">{item?.email}</p>
-            </div>
+          src={item.img || "/noAvatar.png"}
+          alt=""
+          width={40}
+          height={40}
+          className="md:hidden xl:block w-10 h-10 rounded-full object-cover"
+        />
+        <div className="flex flex-col">
+          <h3 className="font-semibold">{item.name}</h3>
+          <p className="text-xs text-gray-500">{item?.email}</p>
+        </div>
       </td>
-      <td className="hidden  md:table-cell">{item.teacherId}</td >
-      <td className="hidden  md:table-cell">{item.teacherId}</td >
-      <td className="hidden  md:table-cell">{item.classes.join(",")}</td >
-      <td className="hidden  lg:table-cell">{item.phone}</td >
-      <td className="hidden  lg:table-cell">{item.address}</td>
+      <td className="hidden md:table-cell">{item.username}</td>
+      <td className="hidden md:table-cell">
+        {item.subjects.map((subject) => subject.name).join(",")}
+      </td>
+      <td className="hidden md:table-cell">
+        {item.classes.map((classItem) => classItem.name).join(",")}
+      </td>
+      <td className="hidden md:table-cell">{item.phone}</td>
+      <td className="hidden md:table-cell">{item.address}</td>
       <td>
         <div className="flex items-center gap-2">
           <Link href={`/list/teachers/${item.id}`}>
-          <button className="w-12 h-12 flex items-center justify-center rounded-full">
-              <Image src="/view.png" alt="" width={20} height={20}/>
-          </button>
+            <button className="w-7 h-7 flex items-center justify-center rounded-full bg-lamaSky">
+              <Image src="/view.png" alt="" width={16} height={16} />
+            </button>
           </Link>
           {role === "admin" && (
-          <FormModal table="teacher" type="delete" id={item.id}/>
-          )} 
+            // <button className="w-7 h-7 flex items-center justify-center rounded-full bg-lamaPurple">
+            //   <Image src="/delete.png" alt="" width={16} height={16} />
+            // </button>
+            <FormModal table="teacher" type="delete" id={item.id} />
+          )}
         </div>
       </td>
     </tr>
   );
 
+const TeacherListPage = async () => {
+
+      const teacher = await prismaClient.teacher.findMany();
+       
+      console.log(teacher);
 
 
   return (
